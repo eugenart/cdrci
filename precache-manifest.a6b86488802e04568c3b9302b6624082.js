8b945f37dcc6fa2f9f04bc5812e0148dd2ed4149
Eugene <42961046+eugenart@users.noreply.github.com>
2020-05-12 10:39:29 +0300

    Add files via upload

---
+++ precache-manifest.a6b86488802e04568c3b9302b6624082.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5efb70b94d84eac111",
+    "url": "../css/app.1dfad32c.css"
+  },
+  {
+    "revision": "d226ebf76ccbdad6450f",
+    "url": "../css/chunk-6a9e8383.16b64549.css"
+  },
+  {
+    "revision": "e8c4ff6b0c00e6ec149f",
+    "url": "../css/chunk-vendors.a2a6e132.css"
+  },
+  {
+    "revision": "2b70144c8622b4cdedd07929545b9ee2",
+    "url": "../index.html"
+  },
+  {
+    "revision": "7b5efb70b94d84eac111",
+    "url": "../js/app.ed42f662.js"
+  },
+  {
+    "revision": "d226ebf76ccbdad6450f",
+    "url": "../js/chunk-6a9e8383.e5263f79.js"
+  },
+  {
+    "revision": "e8c4ff6b0c00e6ec149f",
+    "url": "../js/chunk-vendors.c15f7fdf.js"
+  },
+  {
+    "revision": "88fa6cb79362c9263930c487b6857e77",
+    "url": "../manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "../robots.txt"
+  }
+]);
